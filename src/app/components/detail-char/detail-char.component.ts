74f63fbe83aec7ad8c1fc7e98e3406b011d6d59e
Sebas <seplarui@gmail.com>
2020-07-27 20:49:40 +0200

    try parameters turn back

--- src/app/components/detail-char/detail-char.component.ts
+++ src/app/components/detail-char/detail-char.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataApiService } from '../../services/data-api.service';
 import { Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-char',
