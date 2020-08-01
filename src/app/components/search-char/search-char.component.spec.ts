51bd62917ed5e75dd0d18672196ff35e0df9ef7a
Sebas <seplarui@gmail.com>
2020-08-01 13:47:35 +0200

    search-char component

---
+++ src/app/components/search-char/search-char.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchCharComponent } from './search-char.component';
+
+describe('SearchCharComponent', () => {
+  let component: SearchCharComponent;
+  let fixture: ComponentFixture<SearchCharComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchCharComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchCharComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/search-char/search-char.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-char',
+  templateUrl: './search-char.component.html',
+  styleUrls: ['./search-char.component.css']
+})
+export class SearchCharComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
