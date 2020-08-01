34403ad07ad952dc37aca21719683e267850f1a6
Sebas <seplarui@gmail.com>
2020-08-01 13:59:06 +0200

    delete search char component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
@@ -11,5 +12,10 @@ export class NavComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  searchChar(formSearch: NgForm) {
 
+    console.log(formSearch.value);
+
+
+  }
 }
