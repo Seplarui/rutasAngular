bf9652786d18cd6c378962c9518d73fab7b5aa40
Sebas <seplarui@gmail.com>
2020-08-02 20:00:58 +0200

    add search-characters component

---
+++ src/app/components/search-characters/search-characters.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchCharactersComponent } from './search-characters.component';
+
+describe('SearchCharactersComponent', () => {
+  let component: SearchCharactersComponent;
+  let fixture: ComponentFixture<SearchCharactersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchCharactersComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchCharactersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
